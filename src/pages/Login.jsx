8723492f243ea115c4d3deb98d7457fa680ad227
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-03 05:13:48 +0530

    install tailwind

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="h-screen bg-red-500">Login
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
   )
 }
 
