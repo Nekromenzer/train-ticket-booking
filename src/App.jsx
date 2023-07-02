ca4c5547c30252d86635ad380bf279174429cb15
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-02 19:31:04 +0530

    basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
 import { ConfigProvider } from 'antd'
-import Sample from './components/sample'
 import { antThemeConfig } from '../antThemeConfig'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route
+} from 'react-router-dom'
+
+import { Error, Home, Login } from './pages'
+import HandleAuthRedirect from './auth/HandleAuthRedirect'
+import Dashboard from './components/Dashboard'
 
 function App () {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Dashboard />}>
+        <Route element={<HandleAuthRedirect />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+      </Route>
+    )
+  )
   return (
     <ConfigProvider theme={antThemeConfig}>
-      <div>
-        <Sample />
-      </div>
+      <RouterProvider router={router} />
     </ConfigProvider>
   )
 }
---
+++ src/auth/HandleAuthRedirect.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router'
+
+const HandleAuthRedirect = () => {
+  // need to add real routed here
+  const isAuthenticated = true
+  if (!isAuthenticated) return <Navigate to='login' />
+  return <Outlet />
+}
+
+export default HandleAuthRedirect
