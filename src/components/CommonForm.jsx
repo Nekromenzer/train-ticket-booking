c1f632a15a5398dbe559f0864875fef08a185d37
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-03 14:29:37 +0530

    form refactor

--- src/components/CommonForm.jsx
+++ src/components/CommonForm.jsx
@@ -1,10 +1,24 @@
 import { Form, Input } from 'antd'
+import CommonBtn from './CommonBtn'
 
 const CommonForm = props => {
-  const { fields, name, layout = 'vertical', className, type } = props
+  const {
+    fields,
+    name,
+    layout = 'vertical',
+    className,
+    type,
+    signInText,
+    signUpText,
+    formBtnText
+  } = props
 
-  const renderInput = fieldType => {
-    return fieldType === 'password' ? <Input.Password /> : <Input />
+  const renderInput = (fieldType, placeholder) => {
+    return fieldType === 'password' ? (
+      <Input.Password placeholder={placeholder} className='font-raleway' />
+    ) : (
+      <Input placeholder={placeholder} className='font-raleway' />
+    )
   }
 
   const renderItemsInArray = () => {
@@ -17,14 +31,29 @@ const CommonForm = props => {
       label={item.label}
       name={item.name}
       rules={item.rules}
+      hasFeedback={item.hasFeedback}
+      className='font-raleway my-8'
     >
-      {renderInput(item.type)}
+      {renderInput(item.type, item.placeholder)}
     </Form.Item>
   ))
 
   return (
     <Form name={name} layout={layout} className={className}>
       {formItems}
+      <Form.Item>
+        <CommonBtn
+          type='primary'
+          htmlType='submit'
+          classNames='w-full mt-4 bg-blue-400 font-bold'
+        >
+          {name === 'login'
+            ? type === 'signIn'
+              ? signInText
+              : signUpText
+            : formBtnText}
+        </CommonBtn>
+      </Form.Item>
     </Form>
   )
 }
