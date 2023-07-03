c13570d4739dc36a4de735c4fedc8e6fda9fd55c
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-03 16:57:46 +0530

    eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    'react/display-name': 'off'
   }
 }
