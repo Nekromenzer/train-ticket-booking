06101ed88432fbef3d6af3b5a57af98eba6a6875
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-14 02:17:52 +0530

    common api function ok

--- src/api/handleApiCall.jsx
+++ src/api/handleApiCall.jsx
@@ -1,14 +1,43 @@
 import axios from 'axios'
 import urlDoc from './url'
 
-const api = import.meta.env.VITE_API_URL
+const baseUrl = import.meta.env.VITE_API_URL
 
-const handleApiCall = ({ variant, urlType }) => {
-  const url = `${api}${urlDoc[variant][urlType].url}`
+const handleApiCall = ({
+  variant = 'user',
+  urlType,
+  data,
+  params,
+  cb = returnData => {
+    console.log(returnData, 'default cb')
+  },
+  setLoading = state => {
+    console.log(state, 'default setLoading')
+  }
+}) => {
+  const url = `${baseUrl}${urlDoc[variant][urlType].url}`
   const type = urlDoc[variant][urlType].type
 
-  console.log(type, url)
-  return <div>handleApiCall</div>
+  async function handelCall () {
+    setLoading(true)
+    try {
+      const response = await axios({
+        method: 'get',
+        url: 'https://fakestoreapi.com/products',
+        data: data,
+        params: params,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      setLoading(false)
+      return cb(response)
+    } catch (error) {
+      setLoading(false)
+      throw error
+    }
+  }
+  return handelCall()
 }
 
 export default handleApiCall
