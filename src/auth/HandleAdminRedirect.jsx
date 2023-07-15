fe9a806d62a7e5a3750f5ca3b1964cef43b2bb59
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-15 22:32:29 +0530

    admin redirect ok

--- src/auth/HandleAdminRedirect.jsx
+++ src/auth/HandleAdminRedirect.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Outlet } from 'react-router'
-
 const adminUrl = import.meta.env.VITE_ADMIN_EMAIL
 
 const HandleAdminRedirect = () => {
+  const loggedUserEmail = localStorage.getItem('train_user_email')
   // need to add real routed here
-  if (adminUrl !== 'admin.trainTrack@gmal.com') return <Navigate to='/' />
+  if (adminUrl !== loggedUserEmail) return <Navigate to='/' />
   return <Outlet />
 }
 
