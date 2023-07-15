5ebd167361f626f21fc61081e36eedfaa54a51eb
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-16 01:20:32 +0530

    file clean

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,13 @@ import { useContext, useEffect } from 'react'
 import { TwoColSideBar } from '../components'
 import appDataContext from '../context/AppDataContext'
 import UserHome from './user/UserHome'
-import { Navigate, useNavigate } from 'react-router-dom'
-import authContext from '../context/AuthContext'
+import { Navigate} from 'react-router-dom'
 
 const loggedUserEmail = localStorage.getItem('train_user_email')
 const adminUrl = import.meta.env.VITE_ADMIN_EMAIL
 
 const Home = () => {
   const [activeTabIndex] = useContext(appDataContext)
-  const [isSystemAdmin] = useContext(authContext)
-
-  const navigate = useNavigate()
   const GetContentForActiveTab = () => {
     if (activeTabIndex === 1) {
       return <UserHome />
--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -1,23 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
-
-const loggedUserEmail = localStorage.getItem('train_user_email')
-const adminUrl = import.meta.env.VITE_ADMIN_EMAIL
-const isAdmin = adminUrl === loggedUserEmail
-
 const AdminHome = () => {
-  const navigate = useNavigate()
-  const loggedUserEmail = localStorage.getItem('train_user_email')
-
-  // useEffect(() => {
-  //   const handleNavigate = () => {
-  //     if (!isAdmin) {
-  //       return navigate('/', { replace: true })
-  //     }
-  //   }
-  //   return handleNavigate()
-  // }, [loggedUserEmail, navigate, loggedUserEmail])
   return <div>Admin Home</div>
 }
 
