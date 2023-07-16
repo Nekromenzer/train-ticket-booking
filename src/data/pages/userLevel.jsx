137bfa0a56e4f0531852d536bb9793fc66f442e3
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-16 18:34:48 +0530

    data design fix

--- src/data/pages/userLevel.jsx
+++ src/data/pages/userLevel.jsx
@@ -21,24 +21,44 @@ const data = {
       badge:
         'https://img.icons8.com/external-justicon-lineal-color-justicon/64/external-medal-awards-justicon-lineal-color-justicon-1.png',
       discount: '5%',
-      description:
-        'You are a Silver member of our service, to become Gold member you need to book 10 tickets, each ticket will give you 1 point'
+      description: (
+        <p className='text-justify text-base leading-8'>
+          You are a Silver member of our service, to become Gold member you need
+          to book 10 tickets,{' '}
+          <span className='text-base font-roboto italic text-black'>
+            each ticket will give you 1 point
+          </span>
+        </p>
+      )
     },
     {
       name: 'Gold',
       badge:
         'https://img.icons8.com/external-justicon-lineal-color-justicon/64/external-medal-awards-justicon-lineal-color-justicon-2.png',
       discount: '10%',
-      description:
-        'You are a Gold member of our service, to become Platinum member you need to book 15 tickets, each ticket will give you 1 point'
+      description: (
+        <p className='text-justify text-base leading-8'>
+          You are a Gold member of our service, to become Platinum member you
+          need to book 15 tickets,{' '}
+          <span className='text-base font-roboto italic text-black'>
+            each ticket will give you 1 point
+          </span>
+        </p>
+      )
     },
     {
       name: 'Platinum',
       badge:
         'https://img.icons8.com/external-justicon-lineal-color-justicon/64/external-medal-awards-justicon-lineal-color-justicon-3.png',
       discount: '15%',
-      description:
-        'You are a Platinum member of our service, you have 15% of discount for all tickets'
+      description: (
+        <p className='text-justify text-base leading-8'>
+          You are a Platinum member of our service,{' '}
+          <span className='text-base font-roboto italic text-black'>
+            you have 15% of discount for all tickets
+          </span>
+        </p>
+      )
     }
   ]
 }
