32a925a8d6c252ec7d1e5be6dd2055875ed74a35
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-21 14:35:57 +0530

    add urls

--- src/api/url.jsx
+++ src/api/url.jsx
@@ -1,8 +1,15 @@
 const urlDoc = {
-  user: {
+  auth: {
     login: { url: '/login', type: 'post' },
     logout: { url: '/logout', type: 'post' },
-    register: { url: '/register', type: 'post' }
+    register: { url: '/register', type: 'post' },
+    // email verify
+    sendVerification: { url: '/send-verification-email', type: 'post' },
+    verifyEmail: { url: '/verify-email', type: 'post' },
+    // forget password
+    forgetPassword: { url: '/forgot-password', type: 'post' },
+    resetPassword: { url: '/reset-password', type: 'post' },
+    verifyToken: { url: '/verify-token', type: 'post' }
   },
   serDashboard: {
     search: { url: '/dashboard/search', type: 'post' },
