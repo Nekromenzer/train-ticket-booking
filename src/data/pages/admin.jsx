721bbb7664ba4012e36a5a8ffd7dbe8e3ef012e9
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-07-26 20:40:20 +0530

    admin stta data

--- src/data/pages/admin.jsx
+++ src/data/pages/admin.jsx
@@ -21,31 +21,24 @@ const data = {
       header: 'Top 3 routes',
       description: 'Most used routes all the time',
       //   need to be changed
-      tags: [
-        { name: 'Kandy - Badulla', color: 'red' },
-        { name: 'Colombo - Kandy', color: 'green' },
-        { name: 'Badulla - Ella', color: 'blue' }
-      ],
-      type: 'tags'
+      tags: ({ routes = [] }) => routes,
+      type: 'tags',
+      color: 'red'
     },
     {
-      header: 'Top 3 Destinations',
-      description: 'Most famous destination',
-      tags: [
-        { name: 'Kandy', color: 'red' },
-        { name: 'Badulla', color: 'green' },
-        { name: 'Ella', color: 'blue' }
-      ],
-      type: 'tags'
+      header: 'Top Classes',
+      description: 'Top classes in last two months',
+      tags: ({ destinations = [] }) => destinations,
+      type: 'common-tags'
     },
     {
       header: 'Revenue Increment',
       description: 'Revenue Increase compare to last month',
       type: 'liq-chart',
-      props: {
-        percent: 0.25,
+      props: ({ percentage = 0 }) => ({
+        percent: percentage,
         height: 20
-      }
+      })
     },
     {
       header: 'System',
