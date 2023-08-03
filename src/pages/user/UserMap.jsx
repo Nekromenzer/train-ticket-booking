4c2a1a8d27b4c313c964f7e6ed660e82571141e7
Nekromenzer <bamaneeshagimshan1999@gmail.com>
2023-08-03 10:02:55 +0530

    live location ok

--- src/pages/user/UserMap.jsx
+++ src/pages/user/UserMap.jsx
@@ -2,15 +2,17 @@ import { useEffect, useState } from 'react'
 import handleApiCall from '../../api/handleApiCall'
 import LoadingAnimation from '../../components/elements/LoadingAnimation'
 import { FaLocationArrow } from 'react-icons/fa'
+import dayjs from 'dayjs'
 
 const UserMap = () => {
   const [currentLocation, setCurrentLocation] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   const fetchLocation = () => {
     handleApiCall({
       variant: 'userDashboard',
-      urlType: 'currentLocation',
+      urlType: 'tracking',
+      auth: true,
       setLoading: setLoading,
       cb: (res, sate) => {
         if (sate === 200) {
@@ -24,7 +26,7 @@ const UserMap = () => {
     fetchLocation()
     const interval = setInterval(() => {
       fetchLocation() // API call every 5 minutes
-    }, 5 * 60 * 1000)
+    }, 200000)
 
     return () => {
       return () => {
@@ -36,24 +38,32 @@ const UserMap = () => {
   return (
     <LoadingAnimation loading={loading} tip='Getting live locations...'>
       <div className='rounded-xl h-[16rem] p-2 lg:p-3  backdrop-blur-[0.5px] flex overflow-y-auto  flex-col gap-4 w-fit'>
-        <div className='flex gap-3  bg-green-500/90 h-fit px-2 py-1 items-center rounded-md '>
-          <div className='text-base antialiased font-normal leading-relaxed'>
-            Udarata manike
+        {currentLocation?.length ? (
+          currentLocation.map((item, idx) => (
+            <div
+              key={idx}
+              className={`flex gap-3 ${
+                idx === 0 ? 'bg-green-500/90' : 'bg-white-500/90'
+              } h-fit px-2 py-1 items-center rounded-md`}
+            >
+              <div className='text-base antialiased font-normal leading-relaxed'>
+                {item.route}
+              </div>
+              <div className='text-base antialiased font-normal leading-relaxed'>
+                {dayjs(item.departure_time).format('MM/DD hh:mm')} -{' '}
+                {dayjs(item.departure_time).format('MM/DD hh:mm')}
+              </div>
+              <FaLocationArrow className='fill-blue-800 animate-ping' />
+              <div className='text-base antialiased leading-relaxed font-bold'>
+                {item.current_location}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className='text-base antialiased font-normal text-white leading-relaxed text-center bg-red-500/90 h-fit px-2 py-1 rounded-md'>
+            No live locations available
           </div>
-          <FaLocationArrow className='fill-blue-800 animate-ping' />
-          <div className='text-base antialiased font-normal leading-relaxed'>
-            kandy
-          </div>
-        </div>
-        <div className='flex gap-3 bg-white/80 h-fit px-2 py-1 items-center rounded-md'>
-          <div className='text-base antialiased font-normal leading-relaxed'>
-            Udarata manike
-          </div>
-          <FaLocationArrow className='fill-blue-800' />
-          <div className='text-base antialiased font-normal leading-relaxed'>
-            kandy
-          </div>
-        </div>
+        )}
       </div>
     </LoadingAnimation>
   )
